bd87d678602935bd117df1e9d98c18b9670504c3
ryo-is <ryosuke.izumi62@gmail.com>
2022-05-08 11:33:51 +0900

    feat: add calendar for select date

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,6 @@
 import { Component, createSignal, For } from 'solid-js';
 import dayjs from 'dayjs';
-import { Svg } from './Svg';
-import { DatePicker } from './DatePicker';
+import { Calendar } from './Calendar';
 
 type TimelineItem = {
   date: string;
@@ -10,9 +9,9 @@ type TimelineItem = {
 
 export const Timeline: Component = () => {
   const [timelineItems] = createSignal<TimelineItem[]>([
-    { date: '2022-05-02T07:00:00+09:00', title: '朝ご飯食べた' },
-    { date: '2022-05-02T12:00:00+09:00', title: '昼食食べた' },
-    { date: '2022-05-02T18:00:00+09:00', title: '晩ごはん食べた' },
+    { date: '2022-05-02T07:00:00+09:00', title: '朝食' },
+    { date: '2022-05-02T12:00:00+09:00', title: '昼食' },
+    { date: '2022-05-02T18:00:00+09:00', title: '夕食' },
   ]);
   const [date, setDate] = createSignal(dayjs().format('YYYY-MM-DD'));
 
@@ -21,28 +20,25 @@ export const Timeline: Component = () => {
   };
 
   return (
-    <div>
-      <DatePicker date={date()} setDate={setDate} submit={submitDate} />
-      <ul class="steps steps-vertical">
-        <For each={timelineItems()}>
-          {(item) => (
-            <li data-content="" class="step">
-              <div class="text-left py-4 pl-2">
-                <div>{dayjs(item.date).format('HH:mm')}</div>
-                <div class="text-xl mt-2">{item.title}</div>
-              </div>
-            </li>
-          )}
-        </For>
-        <li data-content="" class="step">
-          <div class="text-left py-4 pl-2">
-            <button class="btn btn-primary no-animation">
-              <Svg name="plus" />
-              <span class="pl-1">追加する</span>
-            </button>
-          </div>
-        </li>
-      </ul>
+    <div class="flex">
+      <div class="mr-10">
+        <Calendar date={date()} setDate={setDate} submit={submitDate} />
+      </div>
+      <div>
+        <div class="mt-4 mb-2 text-lg">Menu of {date()}</div>
+        <ul class="steps steps-vertical">
+          <For each={timelineItems()}>
+            {(item) => (
+              <li data-content="" class="step">
+                <div class="text-left py-4 pl-2">
+                  <div>{dayjs(item.date).format('HH:mm')}</div>
+                  <div class="text-xl mt-2">{item.title}</div>
+                </div>
+              </li>
+            )}
+          </For>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,7 +1,13 @@
-import { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import { Timeline } from '@components/Timeline';
+import { Calendar } from '@components/Calendar';
+import dayjs from 'dayjs';
 
 export const Top: Component = () => {
+  const [date, setDate] = createSignal(dayjs().format('YYYY-MM-DD'));
+
+  const submit = () => {};
+
   return (
     <div class="flex flex-col text-md h-full p-4">
       <Timeline />
